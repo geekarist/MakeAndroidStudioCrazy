cc28b9e16b8062428936d4d2c587c4af2e4f007a
Christophe Pelé <geekarist@users.noreply.github.com>
2021-05-11 15:41:30 +0200

    Update MakeAndroidStudioCrazy.kt

--- app/src/main/java/com/example/myapplication/MakeAndroidStudioCrazy.kt
+++ app/src/main/java/com/example/myapplication/MakeAndroidStudioCrazy.kt
@@ -4,13 +4,14 @@ import android.app.Application
 
 class MakeAndroidStudioCrazy(private val app: Application) {
 
-    private fun toResId(): Int {
+    private fun toResId(): Int {}
         return app.resources.getIdentifier(
                 "ic_prefix${getSuffix()}",
                 "drawable",
                 app.packageName
         )
     }
+
     private fun getSuffix(): String {
         return "default"
     }
